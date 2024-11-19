030bb82ec6c1d4fd2c64799f30413ebfd61d399b
choseongchan3712 <choseongchan3712@naver.com>
2024-11-19 17:07:41 +0900

    date calculate d-day part complete

--- src/pages/date-pages/DDay.tsx
+++ src/pages/date-pages/DDay.tsx
@@ -1,12 +1,81 @@
 import { useContext } from "react";
 import DateBody from "./date-components/DateBody";
 import { DateContext } from "./date-components/DateContext";
+import moment from "moment";
+import "moment/locale/ko";
+import {
+  BeforeContext,
+  CountContext,
+  SecContext,
+} from "./date-components/ValueContext";
 
 const DDay = (): JSX.Element => {
   const {date} = useContext(DateContext)!;
+  const { secValue } = useContext(SecContext)!;
+  const { before } = useContext(BeforeContext)!;
+  const { count } = useContext(CountContext)!;
+
+  const firstCalculating = (value: string): string => {
+    if (moment(value).startOf("day").isAfter(moment().startOf("day"))) {
+      const result: string = `D-${moment(value).diff(moment(), "days") + 1}`;
+      return result;
+    } else if (moment(value).startOf("day").isBefore(moment().startOf("day"))) {
+      const result: string = `D+${
+        moment().diff(moment(value), "days")
+      }`;
+      return result;
+    } else {
+      const result: string = `D-day`;
+      return result;
+    }
+  };
+
+  const firstCalString: string = firstCalculating(date);
+
+  const secCalculating = (value: string): string => {
+    if (moment(value).startOf("day").isAfter(moment(secValue).startOf("day"))) {
+      const result: string = `D-${moment(value).diff(
+        moment(secValue),
+        "days"
+      )}`;
+      return result;
+    } else if (
+      moment(value).startOf("day").isBefore(moment(secValue).startOf("day"))
+    ) {
+      const result: string = `D+${
+        moment(secValue).diff(moment(value), "days")
+      }`;
+      return result;
+    } else {
+      const result: string = `D-day`;
+      return result;
+    }
+  };
+
+  const secondCalString: string = secCalculating(date);
+
+  const countCalculating = (): string => {
+    if (before === "before active") {
+      const result: string = `${moment(date)
+        .subtract(count, "days")
+        .format("LL")}`;
+      return result;
+    } else {
+      const result: string = `${moment(date).add(count, "days").format("LL")}`;
+      return result;
+    }
+  };
+
+  const countingDate: string = countCalculating();
+
   return (
-    // <DateBody date={date} />
-    <></>
+    <DateBody
+      setedDate={date}
+      firstCal={firstCalString}
+      secCal={secondCalString}
+      countedDate={countingDate}
+      whatUnit="일"
+    />
   );
 };
 
